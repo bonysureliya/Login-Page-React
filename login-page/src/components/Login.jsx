60b7b25e0140769835107fb28445a3a1013a1484
Bony <bonysureliya20@gmail.com>
2022-11-24 17:50:39 +0000

    Updated Login Component

--- login-page/src/components/Login.jsx
+++ login-page/src/components/Login.jsx
@@ -1,16 +1,83 @@
 // import './App.css'
-import { Button, Container , ButtonGroup, Link , Text , Flex , Box , Stack } from '@chakra-ui/react'
+import {
+  Button,
+  Container,
+  ButtonGroup,
+  Link,
+  Text,
+  Flex,
+  Box,
+  VStack,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 function Login() {
-  
   return (
     <>
-      <Box>
-        <Flex bgColor={'red.200'} height={'96'} boxShadow={'2xl'} gap={4} justifyContent={'center'} marginRight={'72'} marginLeft={'80'} marginTop={'24'}>
-            <Text>Login</Text>
-         </Flex>
-      </Box>
+      <Stack
+        borderColor={"black"}
+        rounded={"2xl"}
+        position={"absolute"}
+        left={"20%"}
+        bgColor={"gray.200"}
+        height={"auto"}
+        width={"96"}
+        boxShadow={"xl"}
+        gap={4}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        flexDirection={"column"}
+        mx={"80"}
+        my={20}
+        p={6}
+      >
+        <Flex justifyContent={"center"}>
+          <Text fontSize={"3xl"}>Login</Text>
+        </Flex>
+        <Text marginLeft={"4"} marginTop={"2"} fontSize={"xl"}>
+          Email :
+        </Text>
+        <Input
+          marginLeft={"4"}
+          h={10}
+          width={"90%"}
+          size={"md"}
+          placeholder={"Enter an Email"}
+        ></Input>
+        <Text marginLeft={"4"} marginTop={"2"} fontSize={"xl"}>
+          Password
+        </Text>
+        <Input
+          type={"password"}
+          marginLeft={"4"}
+          width={"90%"}
+          size={"md"}
+          h={10}
+          placeholder={"Enter your Password"}
+        ></Input>
+        <Button
+          leftIcon={<FaGoogle />}
+          colorScheme={"facebook"}
+          py={4}
+          w={"60%"}
+          rounded={"lg"}
+        >
+          Continue with Google
+        </Button>
+        <Button
+          leftIcon={<FaGithub />}
+          bgColor={"black"}
+          colorScheme={"whiteAlpha"}
+          py={4}
+          w={"60%"}
+          rounded={"lg"}
+        >
+          Continue with Github
+        </Button>
+      </Stack>
     </>
   );
 }
 
-export default Login
+export default Login;
